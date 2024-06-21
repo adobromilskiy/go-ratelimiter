ac7ff4ea8722327ad84aa464c1530b6b1b0cd99d
Alexander Dobromilskiy <alexdob.twist@gmail.com>
2024-06-21 12:27:13 +0300

    add more test with error

--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -55,6 +55,31 @@ func TestDo_RateLimit(t *testing.T) {
 	}
 
 	if ok {
-		t.Fatalf("unexpected rate limit")
+		t.Fatalf("expected rate limit")
+	}
+}
+
+func TestDo_Error(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+
+	ctx := context.Background()
+	ip := "127.0.0.1"
+	limit := int64(10)
+	duration := time.Minute
+
+	key := fmt.Sprintf("{ip:%s}:requests", ip)
+
+	rl := NewRateLimiter(db)
+
+	mock.ExpectIncr(key).SetVal(11)
+	mock.ExpectExpire(key, duration).SetErr(fmt.Errorf("error"))
+
+	ok, err := rl.Do(ctx, key, limit, duration)
+	if err == nil {
+		t.Fatalf("expected error, but got nil")
+	}
+
+	if ok {
+		t.Fatalf("expected rate limit")
 	}
 }
